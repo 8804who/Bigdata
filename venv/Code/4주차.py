65ce3803f5c50d142674e73671b938f4c4fce156
1a2a3a4a5 <8804who@naver.com>
2022-03-28 18:20:51 +0900

    수업

--- venv/Code/4주차.py
+++ venv/Code/4주차.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import numpy as np
+from pandas import Series, DataFrame
+list_data=[1,2,3,4,5]
+list_name=["a","b","c","d","e"]
+example_obj=Series(data=list_data,index=list_name)
+print(example_obj)
